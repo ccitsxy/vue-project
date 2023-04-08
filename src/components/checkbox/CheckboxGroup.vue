184b9b033c1a5f8bedb9df1ecf590dd085ce6cf2
ccitsxy <1127885451@qq.com>
2023-04-08 23:08:43 +0800

    chore: add stylelint

--- src/components/checkbox/CheckboxGroup.vue
+++ src/components/checkbox/CheckboxGroup.vue
@@ -11,9 +11,7 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-  <div :aria-label="ariaLabel" :class="['c-checkbox-group']" role="list">
+  <div :aria-label="ariaLabel" :class="['c-checkbox-group', `c-checkbox-${direction}`]" role="list">
     <slot />
   </div>
 </template>
-
-<style></style>
